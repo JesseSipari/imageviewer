5aa6ed0106e7db0ca8cb4c4b6d2e3427302189ae
jesse <sipari.jesse@gmail.com>
2026-06-13 11:22:42 +0300

    The test-image displays correctly

--- iv.c
+++ iv.c
@@ -1,9 +1,11 @@
-#include <SDL2/SDL_surface.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_video.h>
+//gcc -Wall -Wextra -g -o  iv iv.c `sdl2-config --cflags --libs`
+//cat test-img.ppm | ./iv
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+
 int main() {
     FILE *in = stdin;
     char *pthrowaway = calloc(1000, sizeof(char));
@@ -11,6 +13,7 @@ int main() {
     fgets(pthrowaway, 1000, in);
 
     // Read the second line (comment)
+    // TODO: Maybe all files don't have a comment!
     fgets(pthrowaway, 1000, in);
     // Read the third line (dimensions: width / height)
     char *pdimensions = calloc(1000, sizeof(char));
@@ -28,15 +31,16 @@ int main() {
     SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0); 
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
-    Uint8 r, g, b;
-    r=0xFF;
-    g=0x00;
-    b=0x00;
-    Uint32 color = SDL_MapRGB(psurface->format, r,g,b);
 
     SDL_Rect pixel = (SDL_Rect){0,0,1,1};
-    for(int x=0; x<width; x++) {
-        for(int y=0; y<height; y++) {
+    Uint32 color = 0; 
+    for(int y=0; y<height; y++) {
+        for(int x=0; x<width; x++) {
+            Uint8 r, g, b;
+            r=(char) getchar();
+            g=(char) getchar();
+            b=(char) getchar();
+            color = SDL_MapRGB(psurface->format, r,g,b);
             pixel.x=x;
             pixel.y=y; 
             SDL_FillRect(psurface, &pixel, color); 
@@ -44,5 +48,14 @@ int main() {
         }
     }
     SDL_UpdateWindowSurface(pwindow);
-    SDL_Delay(3000);
+    int app_running = 1; 
+    while(app_running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                app_running = 0;    
+            }
+        }
+        SDL_Delay(100);
+    }
 }

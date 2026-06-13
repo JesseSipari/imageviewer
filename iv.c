7c09b76bc889fc7c9a77d73a7d81a0d20693512d
jesse <sipari.jesse@gmail.com>
2026-06-13 11:43:33 +0300

    simplified the memory management and added skip to comment lines in the image data

--- iv.c
+++ iv.c
@@ -8,24 +8,23 @@
 
 int main() {
     FILE *in = stdin;
-    char *pthrowaway = calloc(1000, sizeof(char));
+    char buffer[1000];
     // Read the first line (specifier P3 or P6 - ignore here)
-    fgets(pthrowaway, 1000, in);
+    fgets(buffer, sizeof(buffer), in);
 
-    // Read the second line (comment)
-    // TODO: Maybe all files don't have a comment!
-    fgets(pthrowaway, 1000, in);
-    // Read the third line (dimensions: width / height)
-    char *pdimensions = calloc(1000, sizeof(char));
-    fgets(pdimensions, 1000, in);
-    // Read the fourth line (max color value - ignore it)
-    fgets(pthrowaway, 1000, in);
-    free(pthrowaway);
+    // Skip the comments(#) and find the dimensions 
+    do {
+        fgets(buffer, sizeof(buffer), in);
+    } while (buffer[0] == '#');
     int width = -1;
     int height = -1;
-    sscanf(pdimensions, "%d %d\n", &width, &height); 
-    free(pdimensions);
+    sscanf(buffer, "%d %d", &width, &height); 
     printf("width=%d, height=%d\n", width, height);
+
+    // Skip comments and find the max color value
+    do {
+        fgets(buffer, sizeof(buffer), in);
+    } while (buffer[0] == '#');
 
 
     SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0); 

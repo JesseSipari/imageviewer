ac134f5bd1b47f1603014d45ad00e96cbe8c80ae
jesse <sipari.jesse@gmail.com>
2026-08-18 17:58:51 +0300

    made things better

--- iv.c
+++ iv.c
@@ -36,22 +36,22 @@ int main() {
     SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0); 
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
+    SDL_LockSurface(psurface);
 
-    SDL_Rect pixel = (SDL_Rect){0,0,1,1};
-    Uint32 color = 0; 
+    Uint32 *pixels = (Uint32 *)psurface->pixels;
+
     for(int y=0; y<height; y++) {
         for(int x=0; x<width; x++) {
             Uint8 r, g, b;
-            r=(char) getchar();
-            g=(char) getchar();
-            b=(char) getchar();
-            color = SDL_MapRGB(psurface->format, r,g,b);
-            pixel.x=x;
-            pixel.y=y; 
-            SDL_FillRect(psurface, &pixel, color); 
+            r=(Uint8) getchar();
+            g=(Uint8) getchar();
+            b=(Uint8) getchar();
             
+            pixels[(y * psurface->pitch / 4) + x] = SDL_MapRGB(psurface->format, r, g, b);
         }
     }
+    SDL_UnlockSurface(psurface);
+
     SDL_UpdateWindowSurface(pwindow);
     int app_running = 1; 
     while(app_running) {
